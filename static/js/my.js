a613806a5a03c7030cd1efd893bf64a41dda565b
arkturian <ancientrussian@gmail.com>
2016-07-22 14:55:52 +0700

    FB share button for notes + all remote JS is now async

--- static/js/my.js
+++ static/js/my.js
@@ -1,17 +1,17 @@
 
-function change_outer_links_targets (links) {
-  for (var index in links) {
-    if (links[index].hostname != window.location.hostname) {
-      links[index].target = '_blank'; }}
-}
+// function change_outer_links_targets (links) {
+//   for (var index in links) {
+//     if (links[index].hostname != window.location.hostname) {
+//       links[index].target = '_blank'; }}
+// }
 
-function load_fortune_cookie (fortunes) {
-    var random_index = Math.floor( Math.random() * fortunes.length );
-    var fortune_quote     = fortunes[random_index]["quote"];
-    var fortune_book_link = fortunes[random_index]["book_link"];
-    document.getElementById("fortune-cookie").innerHTML    = fortune_quote;
-    document.getElementById("fortune-book-link").innerHTML = fortune_book_link;
-}
+// function load_fortune_cookie (fortunes) {
+//     var random_index = Math.floor( Math.random() * fortunes.length );
+//     var fortune_quote     = fortunes[random_index]["quote"];
+//     var fortune_book_link = fortunes[random_index]["book_link"];
+//     document.getElementById("fortune-cookie").innerHTML    = fortune_quote;
+//     document.getElementById("fortune-book-link").innerHTML = fortune_book_link;
+// }
 
 // var make_articles_clickable = function () {
 //   var articles = document.getElementsByTagName("article");
